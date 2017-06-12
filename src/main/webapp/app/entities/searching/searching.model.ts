06ecb4226499f16dc4b6b9082b8590b58f480151
sb220 <sb220@hdm-stuttgart.de>
2017-06-12 14:50:21 +0200

    Deleted Offering and Searching in Frontend

--- src/main/webapp/app/entities/searching/searching.model.ts
+++
@@ -1,9 +0,0 @@
-import { Student } from '../student';
-export class Searching {
-    constructor(
-        public id?: number,
-        public searchingvalues?: string,
-        public student?: Student,
-    ) {
-    }
-}
